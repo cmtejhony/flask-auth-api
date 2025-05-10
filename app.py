bd9285d51c30863327858a672b580b6e202650b5
João Flach <jpeomelhor@gmail.com>
2025-05-10 18:20:36 -0300

    Final Version 100% funcional com back e front

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from models.user import db, User
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from dotenv import load_dotenv
@@ -10,15 +10,26 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-jwt = JWTManager(app)
 
+jwt = JWTManager(app)
 db.init_app(app)
 
 @app.route('/')
-def home():
-    return {'mensagem': 'API Flask rodando com sucesso!'}
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET'])
+def login_page():
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET'])
+def register_page():
+    return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -29,7 +40,31 @@ def register():
     
     user_exists = User.query.filter_by(username=data['username']).first()
     if user_exists:
-        return jsonify()
+        return jsonify({"message": "Usuário já existe"}), 400
+    
+    user = User(username=data['username'], email=data['email'])
+    user.set_password(data['password'])
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({"message": "Usuário registrado com sucesso!"}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    user = User.query.filter_by(username=data['username']).first()
+    if user and user.check_password(data['password']):
+        access_token = create_access_token(identity=user.id)
+        return jsonify({"access_token": access_token}), 200
+    else:
+        return jsonify({"message": "Credenciais inválidas"}), 401
+    
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    return jsonify(message="Bem vindo à área protegida!")
 
 with app.app_context():
     print("Criando o banco de dados...")
